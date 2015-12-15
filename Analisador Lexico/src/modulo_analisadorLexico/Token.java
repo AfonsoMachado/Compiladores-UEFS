5fa051170825fc6e6f9f957faa4b1c800cf7d4a1
Oto <yo.tamashii@gmail.com>
2015-12-15 02:09:57 -0200

    Javadoc De token e inserção de coluna (em Arquivo tambem)

--- Analisador Lexico/src/modulo_analisadorLexico/Token.java
+++ Analisador Lexico/src/modulo_analisadorLexico/Token.java
@@ -1,64 +1,87 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package modulo_analisadorLexico;
 
+import manipulação_arquivosIO.Arquivo;
+
 /**
+ * Classe destinada ao armazenamento dos tokens encontrados em um código fonte, 
+ * juntamente com o seu determinado valor e sua posição (linha e coluna) no 
+ * arquivo de origem.
  *
- * @author UCHIHA
+ * @author Lucas Carneiro
+ * @author Oto Lopes
+ * 
+ * @see EstruturaLexica
+ * @see Arquivo
  */
 public class Token {
 
     /**
-     *
+     * Token propriamente dito (Palavra Reservada, Identificador ou Operador,
+     * por exemplo).
      */
-    private String tipo;
+    private final String tipo;
+    /**
+     * Lexema referente ao token.
+     */
+    private final String valor;
+    /**
+     * Linha onde se encontra o inicio deste lexema.
+     */
+    private final int linha;
+    /**
+     * Coluna onde se inicia este lexema.
+     */
+    private final int coluna;
 
     /**
-     *
+     * Construtor da classe.
+     
+     * @param tipo O token (Delimitador, Identificador ou Operador, por exemplo)
+     * @param valor Lexema (valor refente ao token)
+     * @param linha Linha de ocorrencia do lexema
+     * @param coluna Coluna onde se inicia o lexema
      */
-    private String valor;
+    public Token(String tipo, String valor, int linha, int coluna) {
 
-    /**
-     *
-     */
-    private int linha;
-
-    /**
-     *
-     * @param tipo
-     * @param valor
-     * @param linha
-     */
-    public Token(String id, String token, int linha) {
-        this.tipo = token;
-        this.valor = id;
+        this.tipo = tipo;
+        this.valor = valor;
         this.linha = linha;
+        this.coluna = coluna;
     }
-
+    
     /**
-     *
-     * @return
+     * Retorna qual o tipo deste token.
+     * 
+     * @return Qual é o token  
      */
     public String getTipo() {
-        return tipo;
+        return this.tipo;
     }
-
+    
     /**
-     *
-     * @return
+     * Retorna o lexema deste token
+     * 
+     * @return O lexema referente a este token
      */
     public String getValor() {
-        return valor;
+        return this.valor;
     }
-
+    
     /**
-     *
-     * @return
+     * Retorna a linha referente a ocorrencia do lexema.
+     * 
+     * @return A linha referente a ocorrencia do lexema
      */
     public int getLinha() {
-        return linha;
+        return this.linha;
+    }
+    
+    /**
+     * Retorna a coluna refente ao inicio do lexema.
+     * 
+     * @return A coluna onde se inicia o lexema
+     */
+    public int getColuna() {
+        return this.coluna;
     }
 }
