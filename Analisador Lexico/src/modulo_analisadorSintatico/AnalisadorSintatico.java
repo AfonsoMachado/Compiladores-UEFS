5cbd284cda7f5d4c86d960a061f33d4ddda4b9f9
jkdrangel <jkdrangel@hotmail.com>
2016-03-29 13:39:04 -0300

    Sintatico até bloco de constantes.
    Melhorar erros.

--- Analisador Lexico/src/modulo_analisadorSintatico/AnalisadorSintatico.java
+++ Analisador Lexico/src/modulo_analisadorSintatico/AnalisadorSintatico.java
@@ -39,65 +39,102 @@ public class AnalisadorSintatico {
     }
 
     private void erroSintatico(String erro) {
-        erros.add(erro);
+        erros.add("Erro na linha " + proximo.getLinha() + ". " + erro);
     }
 
     private void terminal(String esperado) {
         if (proximo == null ? esperado == null : proximo.getValor().equals(esperado)) {
             proximo = proximo();
         } else {
-            erroSintatico("Erro na linha " + proximo.getLinha() + ". Token " + proximo.getValor() + "esperado.");
+            erroSintatico("Token " + esperado + " esperado.");
         }
     }
 
     private void reconheceArquivo() {
         reconheceConstantes();
-        reconheceDeclaracaoVariavel();
+        reconheceVariaveis();
         reconhecePreMain();
     }
 
     private void reconhecePreMain() {
         System.out.println("pre-main");
-        if(proximo!=null){
-        switch (proximo.getValor()) {
-            case "void":
-                reconheceMain();
-                reconheceClasses();
-                break;
-            case "class":
-                reconheceClasse();
-                reconhecePreMain();
-                break;
-            default:
-                erroSintatico("");
-        }
-        }
-        else {
+        if (proximo != null) {
+            switch (proximo.getValor()) {
+                case "void":
+                    reconheceMain();
+                    reconheceClasses();
+                    break;
+                case "class":
+                    reconheceClasse();
+                    reconhecePreMain();
+                    break;
+                default:
+                    erroSintatico("Espera uma classe ou o método main");
+            }
+        } else {
             erroSintatico("Fim de arquivo inesperado");
         }
     }
 
-    private void reconheceDeclaracaoVariavel() {
-        /*
-         case "const":
-         reconheceConst();
-         break;
-         case "char":
-         reconheceDeclaracaoVariavel();
-         break;
-         case "int":
-         reconheceDeclaracaoVariavel();
-         break;
-         case "bool":
-         reconheceDeclaracaoVariavel();
-         break;
-         case "string":
-         reconheceDeclaracaoVariavel();
-         break;
-         case "float":
-         reconheceDeclaracaoVariavel();
-         break;
-         */
+    private void reconheceClasses() {
+        switch (proximo.getValor()) {
+            case "class":
+                reconheceClasse();
+                reconheceClasses();
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void reconheceConstantes() {
+        System.out.println("constantes");
+        switch (proximo.getValor()) {
+            case "const":
+                reconheceConst();
+                reconheceConstantes();
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void reconheceVariaveis() {
+        System.out.println("variaveis");
+        switch (proximo.getValor()) {
+            case "char":
+                reconheceDeclaracaoVariavel();
+                reconheceVariaveis();
+                break;
+            case "int":
+                reconheceDeclaracaoVariavel();
+                reconheceVariaveis();
+                break;
+            case "bool":
+                reconheceDeclaracaoVariavel();
+                reconheceVariaveis();
+                break;
+            case "string":
+                reconheceDeclaracaoVariavel();
+                reconheceVariaveis();
+                break;
+            case "float":
+                reconheceDeclaracaoVariavel();
+                reconheceVariaveis();
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void reconheceMain() {
+        terminal("void");
+        terminal("main");
+        terminal("(");
+        terminal(")");
+        terminal("{");
+        reconheceConteudoMetodo();
+        terminal("}");
     }
 
     private void reconheceClasse() {
@@ -113,56 +150,7 @@ public class AnalisadorSintatico {
                 terminal("}");
                 break;
             default:
-                break;
-        }
-    }
-
-    private void reconheceConteudoMetodo() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    private void reconheceMain() {
-        terminal("void");
-        terminal("main");
-        terminal("(");
-        terminal(")");
-        terminal("{");
-        reconheceConteudoMetodo();
-        terminal("}");
-    }
-
-    private void reconheceConstantes() {
-        System.out.println("constantes");
-        switch (proximo.getValor()) {
-            case "const":
-                reconheceConst();
-                reconheceConstantes();
-                break;
-            default:
-                break;
-        }
-    }
-
-    private void reconheceClasses() {
-        switch (proximo.getValor()) {
-            case "class":
-                reconheceClasse();
-                reconheceClasses();
-                break;
-            default:
-                break;
-        }
-    }
-
-    private void reconheceConst() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    private void Tipo(String esperado) {
-        if (proximo == null ? esperado == null : proximo.getTipo().equals(esperado)) {
-            proximo = proximo();
-        } else {
-            erroSintatico("Erro na linha " + proximo.getLinha() + ". Token do tipo" + proximo.getTipo() + "esperado.");
+                erroSintatico("Classe com erro.");
         }
     }
 
@@ -175,13 +163,138 @@ public class AnalisadorSintatico {
                 break;
             default:
                 break;
-
         }
 
     }
 
     private void reconheceConteudoClasse() {
-        return;
+        System.out.println("ConteudoClasse");
+        switch (proximo.getValor()) {
+            case "const":
+                reconheceConst();
+                reconheceConteudoClasse();
+                break;
+            case "void":
+                reconheceIdDeclaracao();
+                reconheceConteudoClasse();
+                break;
+            default:
+                if (proximo.getTipo().equals("palavra_reservada") || proximo.getTipo().equals("id")) {
+                    reconheceIdDeclaracao();
+                    reconheceConteudoClasse();
+                    break;
+                }
+                break;
+        }
+    }
+
+    private void reconheceConst() {
+        System.out.println("Const");
+        switch (proximo.getValor()) {
+            case "const":
+                terminal("const");
+                terminal("{");
+                reconheceBlocoConstantes();
+                terminal("}");
+            default:
+                erroSintatico("Esperava um bloco de contantes");
+                break;
+        }
+    }
+
+    private void reconheceBlocoConstantes() {
+        System.out.println("blococonstantes");
+        switch (proximo.getValor()) {
+            case "char":
+                terminal("char");
+                reconheceListaConst();
+                break;
+            case "int":
+                terminal("int");
+                reconheceListaConst();
+                break;
+            case "bool":
+                terminal("bool");
+                reconheceListaConst();
+                break;
+            case "string":
+                terminal("string");
+                reconheceListaConst();
+                break;
+            case "float":
+                terminal("float");
+                reconheceListaConst();
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void reconheceListaConst() {
+        Tipo("id");
+        terminal("=");
+        reconheceAtribuicaoConstante();
+        reconheceAuxiliarDeclaracao();
+
+    }
+
+    private void reconheceAtribuicaoConstante() {
+        switch (proximo.getTipo()) {
+            case "numero":
+                Tipo("numero");
+                break;
+            case "cadeia_constante":
+                Tipo("cadeia_constante");
+                break;
+            case "caracter_constante":
+                Tipo("caracter_constante");
+                break;
+            default:
+                if(proximo.getValor().equals("true")){
+                    terminal("true");
+                }else if(proximo.getValor().equals("false")){
+                    terminal("false");
+                }else {
+                    erroSintatico("Atribuição invalida");
+                }
+                break;
+        }
+    }
+
+    private void reconheceAuxiliarDeclaracao() {
+        switch (proximo.getValor()) {
+            case ",":
+                terminal(",");
+                reconheceListaConst();
+                break;
+            case ";":
+                terminal(";");
+                reconheceBlocoConstantes();
+            default:
+                erroSintatico(", ou ; esperados");
+                break;
+
+        }
+    }
+
+    private void reconheceDeclaracaoVariavel() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private void reconheceConteudoMetodo() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private void Tipo(String esperado) {
+        if (proximo == null ? esperado == null : proximo.getTipo().equals(esperado)) {
+            proximo = proximo();
+        } else {
+            erroSintatico("Erro na linha " + proximo.getLinha() + ". Token do tipo" + proximo.getTipo() + "esperado.");
+        }
+    }
+
+    private void reconheceIdDeclaracao() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
 }
